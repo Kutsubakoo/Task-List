32bda84c46f86397b3ec927e5d0fb25c6cf66179
Angelo Miguel Orticio <angelomiguelorticio@gmail.com>
2025-03-27 23:28:50 +0800

    Established connection to DataBase

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+from db import getTaskList
 
 app = Flask(__name__)
 
-tasklist = [["Walk Dog", True], ["Wash Dishes", False], ["Take out Trash", True]]
+# tasklist = [["Walk Dog", True], ["Wash Dishes", False], ["Take out Trash", True]]
+tasklist = getTaskList()
+
 @app.route("/")
 def home():
     return render_template("tasklist.html", TaskList=tasklist)
