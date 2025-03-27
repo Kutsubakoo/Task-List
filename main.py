b7d8d8a2f069ef64fc767faf329aa45417d96e48
Angelo Miguel Orticio <angelomiguelorticio@gmail.com>
2025-03-27 23:09:12 +0800

    Version 1

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+tasklist = [["Walk Dog", True], ["Wash Dishes", False], ["Take out Trash", True]]
+@app.route("/")
+def home():
+    return render_template("tasklist.html", TaskList=tasklist)
+
+app.run()
